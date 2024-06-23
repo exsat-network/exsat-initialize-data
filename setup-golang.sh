5a84b296eff364ec1a14169a6ff3f3df4a0d13c9
Purson <purson@Pursons-Mac-Studio.local>
2024-06-23 13:07:41 +0800

    fix golang script

--- setup-golang.sh
+++ setup-golang.sh
@@ -7,18 +7,20 @@ sudo apt upgrade -y
 # Install required dependencies
 sudo apt install -y curl git mercurial make binutils bison gcc build-essential
 
+# Install Go using apt to satisfy gvm's initial requirements
+sudo apt install -y golang
+
 # Install GVM
 bash < <(curl -sSL https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer)
 
 # Source GVM script
 source ~/.gvm/scripts/gvm
 
-# Install Go 1.19 (or any other version you need)
-gvm install go1.19
+# Install Go 1.20 (or any other version you need)
+gvm install go1.20
 
 # Set the default Go version
-gvm use go1.19 --default
-
+gvm use go1.20 --default
 
 # Verify Go installation
 go version
